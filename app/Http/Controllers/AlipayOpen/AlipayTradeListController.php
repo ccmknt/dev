ae919ed0f4d999e882a52775fd394ec1c66eac93
umxnt <youdreams@qq.com>
2016-12-29 10:20:35 +0800

    修复交易订单bug(订单号存在数据库但是支付宝不存在)

--- app/Http/Controllers/AlipayOpen/AlipayTradeListController.php
+++ app/Http/Controllers/AlipayOpen/AlipayTradeListController.php
@@ -20,31 +20,45 @@ class AlipayTradeListController extends AlipayOpenController
     public function index(Request $request)
     {
         $query = AlipayTradeQuery::orderBy('created_at', 'desc')->get();
+        //没有订单
         if ($query->isEmpty()) {
-            $paginator="";
-            $datapage="";
-        } else {
+            $paginator = "";
+            $datapage = "";
+        }
+        //有订单
+        else {
             $query = $query->toArray();
             foreach ($query as $k => $v) {
                 $data1 = json_decode($this->QueryStatus($v['trade_no']), true);
-                $data2 = $data1[$k] = $v;
-                $data[] = array_merge($data1, $data2);
+                //有效订单
+                if ($data1['code'] == "10000") {
+                    $data2 = $data1[$k] = $v;
+                    $data[] = array_merge($data1, $data2);
+
+                    //非数据库模型自定义分页
+                    $perPage = 9;//每页数量
+                    if ($request->has('page')) {
+                        $current_page = $request->input('page');
+                        $current_page = $current_page <= 0 ? 1 : $current_page;
+                    } else {
+                        $current_page = 1;
+                    }
+                    $item = array_slice($data, ($current_page - 1) * $perPage, $perPage); //注释1
+                    $total = count($data);
+                    $paginator = new LengthAwarePaginator($item, $total, $perPage, $current_page, [
+                        'path' => Paginator::resolveCurrentPath(),
+                        'pageName' => 'page',
+                    ]);
+                    $datapage = $paginator->toArray()['data'];
+
+                }
+                //无效订单
+                else {
+                    $paginator = "";
+                    $datapage = "";
+                }
             }
-            //非数据库模型自定义分页
-            $perPage = 9;//每页数量
-            if ($request->has('page')) {
-                $current_page = $request->input('page');
-                $current_page = $current_page <= 0 ? 1 : $current_page;
-            } else {
-                $current_page = 1;
-            }
-            $item = array_slice($data, ($current_page - 1) * $perPage, $perPage); //注释1
-            $total = count($data);
-            $paginator = new LengthAwarePaginator($item, $total, $perPage, $current_page, [
-                'path' => Paginator::resolveCurrentPath(),
-                'pageName' => 'page',
-            ]);
-            $datapage = $paginator->toArray()['data'];
+
         }
         return view('admin.alipayopen.alipaytradelist', compact('paginator', 'datapage'));
     }
